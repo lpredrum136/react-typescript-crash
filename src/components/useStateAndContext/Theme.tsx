a424db2b3e026f94c93363a73aa259f01b7b5aa6
Henry Nguyen <nguyenhai1591@gmail.com>
2021-02-15 22:14:58 +1000

    Change theme

--- src/components/useStateAndContext/Theme.tsx
+++ src/components/useStateAndContext/Theme.tsx
@@ -1,9 +1,34 @@
-import { useContext } from 'react'
+import { ChangeEvent, MouseEvent, useContext, useState } from 'react'
 import { ThemeContext } from '../../contexts/ThemeContext'
 
 const Theme = () => {
+  const [userTheme, setUserTheme] = useState('')
+
   const { theme, toggleTheme } = useContext(ThemeContext)!
-  return <div>{theme}</div>
+
+  const onThemeInput = (event: ChangeEvent<HTMLInputElement>): void => {
+    setUserTheme(event.target.value)
+  }
+
+  const onThemeSubmit = (event: MouseEvent): void => {
+    toggleTheme(userTheme)
+  }
+
+  return (
+    <div>
+      Current theme: {theme}
+      <br />
+      Change theme to:
+      <input
+        type='text'
+        name='theme'
+        placeholder='Enter theme'
+        value={userTheme}
+        onChange={onThemeInput}
+      />
+      <button onClick={onThemeSubmit}>Change Theme!</button>
+    </div>
+  )
 }
 
 export default Theme
